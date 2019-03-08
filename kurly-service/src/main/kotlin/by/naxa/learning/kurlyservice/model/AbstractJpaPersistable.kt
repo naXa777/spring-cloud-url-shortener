d6524a3fa4b3d8af7299fadd3a1a20e3d8c406aa
naXa! <naxa@nextmail.ru>
2019-03-08 23:06:41 +0300

    :card_file_box: add MySQL profile; change ID type to auto incremented; save link creation date

--- kurly-service/src/main/kotlin/by/naxa/learning/kurlyservice/model/AbstractJpaPersistable.kt
+++ kurly-service/src/main/kotlin/by/naxa/learning/kurlyservice/model/AbstractJpaPersistable.kt
@@ -2,18 +2,15 @@ package by.naxa.learning.kurlyservice.model
 
 import java.io.Serializable
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 abstract class AbstractJpaPersistable<T : Serializable> {
 
-    companion object {
-        private val serialVersionUID = -5554308939380869754L
-    }
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: T? = null
 
     override fun toString() = "Entity of type ${this.javaClass.name} with id: $id"
--- kurly-service/src/main/kotlin/by/naxa/learning/kurlyservice/model/Link.kt
+++ kurly-service/src/main/kotlin/by/naxa/learning/kurlyservice/model/Link.kt
@@ -1,17 +1,23 @@
 package by.naxa.learning.kurlyservice.model
 
 import org.hibernate.validator.constraints.URL
+import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Lob
+
+const val MAX_URL_LENGTH = 200 * 1024   // 200 KB
 
 @Entity
 data class Link (
         @field:URL
         @field:Lob
-        @field:Column(nullable = false)
+        @field:Column(nullable = false, length = MAX_URL_LENGTH, columnDefinition = "mediumtext")
         var originalUrl: String,
 
         @field:Column(length = 30, unique = true)
-        var shortCode: String? = null
+        var shortCode: String? = null,
+
+        @field:Column(nullable = false, updatable = false)
+        var creationDate: LocalDateTime = LocalDateTime.now()
 ) : AbstractJpaPersistable<Long>()
