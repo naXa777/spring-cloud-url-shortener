c1376c7feebfd7b6afa3fa31107623a4588a9740
naXa! <naxa@nextmail.ru>
2019-03-10 07:57:49 +0300

    :green_heart: fixing CI build: Turbine context init failed due to missing Eureka Client bean

--- turbine/src/main/kotlin/by/naxa/learning/turbine/TurbineApplication.kt
+++ turbine/src/main/kotlin/by/naxa/learning/turbine/TurbineApplication.kt
@@ -2,9 +2,11 @@ package by.naxa.learning.turbine
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.turbine.EnableTurbine
 
 @EnableTurbine
+@EnableEurekaClient
 @SpringBootApplication
 class TurbineApplication
 
