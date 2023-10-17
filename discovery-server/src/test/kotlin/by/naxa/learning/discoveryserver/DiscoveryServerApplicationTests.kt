62d40004dda6f0bab44389486e38aff3e666b137
Pavel <naxa@nextmail.ru>
2023-10-17 02:38:48 +0200

    :whale: Update Dockerfiles

--- discovery-server/src/test/kotlin/by/naxa/learning/discoveryserver/DiscoveryServerApplicationTests.kt
+++ discovery-server/src/test/kotlin/by/naxa/learning/discoveryserver/DiscoveryServerApplicationTests.kt
@@ -1,13 +1,32 @@
 package by.naxa.learning.discoveryserver
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DiscoveryServerApplicationTests {
+
+	@LocalServerPort
+	private val port: Int = 0
+
+	@Autowired
+	lateinit var testRestTemplate: TestRestTemplate
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun `actuator health endpoint is exposed`() {
+		val url = "http://localhost:$port/actuator/health"
+		val response: ResponseEntity<String> = testRestTemplate.getForEntity(url, String::class.java)
+
+		assertEquals(HttpStatus.OK, response.statusCode)
+	}
 }
